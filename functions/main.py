43ed6377f61cbc87a689a80abdf3ae01b2c5aea7
Pedro <pamaral650@gmail.com>
2024-02-12 14:10:53 -0300

    change

--- functions/main.py
+++ functions/main.py
@@ -8,7 +8,7 @@ headers = {
     "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY5MGFhOTczLTBkZGItNDhiZC1hMDZmLTkwNTMwOTM4NzZiZSIsImlhdCI6MTcwNzE3NTY2NCwic3ViIjoiZGV2ZWxvcGVyLzc1MDJkNGRlLTI2YWEtZjhjMS0wMDRmLWVmZWJjYWFmODVlNSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.HNmCO6end3HxRkhkssTqK2-ZSVXEXQhUcfEuO2vH70vAWPvokFfulZK4WdjHtNbnf72J4ZSmOZb9Av3pV62VSw",
 }
 
-db_connection = mysql.connector.connect(user="remote", database="clashroyale")
+db_connection = mysql.connector.connect(user="remote", database="clashroyale", port=3306)
 cursor = db_connection.cursor()
 
 def main():
